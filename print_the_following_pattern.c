0dfb6c8c9c0338198ca9159265833dab7ac26791
vatturisrinivas <119840114+vatturisrinivas@users.noreply.github.com>
2022-12-26 21:26:52 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     for(int i=1;i<=a;i++)     {         for(int j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,j,n;
+    int a;
     scanf("%d",&a);
-    n=a;
-    for(i=1;i<=a;i++)
+    for(int i=1;i<=a;i++)
     {
-        for(j=1;j<=n;j++)
+        for(int j=1;j<=i;j++)
         {
             printf("%d",j);
         }
         printf("
 ");
-        n=a-i;
     }
 }
