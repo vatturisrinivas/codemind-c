61e0ec6f964f2d9b3fafab5b8454cf4884f38f83
vatturisrinivas <119840114+vatturisrinivas@users.noreply.github.com>
2023-01-03 10:53:32 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     int b[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         scanf("%d",&b[i]);     }     for(int i=0;i<n;i++)     {         printf("%d ",a[i]+b[i]);     } }

---
+++ Monk_and_Welcome_Problem.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int b[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]+b[i]);
+    }
+}
