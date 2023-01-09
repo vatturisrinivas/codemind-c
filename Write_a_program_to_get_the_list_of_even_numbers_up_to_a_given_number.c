ae9d6cd6b919fdbae408d68cd1c502bf951099c3
vatturisrinivas <119840114+vatturisrinivas@users.noreply.github.com>
2023-01-09 23:57:43 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i++)     {         if(i%2==0)         {             printf("%d ",i);
            } 
            
        }
        
    }

--- Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
+++ Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
@@ -5,9 +5,12 @@ int main()
     scanf("%d%d",&a,&b);
     for(i=a;i<=b;i++)
     {
-        if(i%2==0)
+        if(i%2==0) 
         {
-            printf("%d ",i);
-        }
+            printf("%d ",i); 
+            
+        } 
+        
     }
+    
 }
