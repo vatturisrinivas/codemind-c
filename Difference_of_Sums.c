b87eb3df871ff60c654d0c6a7d2d05d3c1a08097
vatturisrinivas <119840114+vatturisrinivas@users.noreply.github.com>
2023-01-15 23:01:19 +0530

    #include<stdio.h> int main() {     int a,i,s=0,g,k=0;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         s=s+i*i; k=k+i;     }     g=k*k;     printf("%d",g-s);
    }

--- Difference_of_Sums.c
+++ Difference_of_Sums.c
@@ -3,11 +3,11 @@ int main()
 {
     int a,i,s=0,g,k=0;
     scanf("%d",&a);
-    for(i=1;i<=a;i++) 
+    for(i=1;i<=a;i++)
     {
         s=s+i*i; k=k+i;
     }
-    g=k*k;
+    g=k*k; 
     printf("%d",g-s);
     
 }
